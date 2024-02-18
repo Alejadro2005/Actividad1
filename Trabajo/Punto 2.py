9c1d83b7e1df901ab5853f9a6b9de9b445e399c3
Alejandro <acaro729@soyudemedellin.edu.co>
2024-02-18 12:39:19 -0500

    segundo commit

---
+++ Trabajo/Punto 2.py
@@ -0,0 +1,11 @@
+def calcular_area(radio):
+    pi = 3.14
+    area = pi * radio**2
+    return area
+
+#Ejemplo de implementacion
+radio_de_circulo_dado = 8
+
+resultado = calcular_area(radio_de_circulo_dado)
+
+print("El area del circulo es", resultado)
