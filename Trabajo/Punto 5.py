1c22bab723e3d5869b734b2bce25dc71c310d3e5
Alejandro <acaro729@soyudemedellin.edu.co>
2024-02-18 12:42:43 -0500

    Quinto commit

---
+++ Trabajo/Punto 5.py
@@ -0,0 +1,2 @@
+temperatura_fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+print(temperatura_fahrenheit, "grados Fahrenheit son equivalentes a", (temperatura_fahrenheit - 32) * 5/9, "grados Celsius")
